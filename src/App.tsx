3cf6150ec00e3e963958691a229fb6af8dc77ae4
DOOZBOI <116702374+DOOZBOI@users.noreply.github.com>
2026-09-21 18:19:13 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -158,7 +158,7 @@ function App() {
       portfolioSectionRef.current.style.willChange = 'transform';
     }
     gsap.to(portfolioSectionRef.current, {
-      y: () => isMobileView ? -window.innerHeight * 0.85 : 900,
+      y: () => isMobileView ? -window.innerHeight * 0.85 : -100,
       scrollTrigger: { trigger: portfolioSectionRef.current, start: "top bottom", end: "bottom top", scrub: isMobileView ? 3 : 2 }
     });
 
